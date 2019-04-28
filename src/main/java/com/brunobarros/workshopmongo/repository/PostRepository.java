ff9bbceb97969dd1a884664e51efefc13463355d
Bruno-BarrosBM <bruno_abarros@hotmail.com>
2019-04-28 11:05:19 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/brunobarros/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.brunobarros.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brunobarros.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
