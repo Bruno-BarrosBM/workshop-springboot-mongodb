bd0fa10039f27223906570e10ccb33d0ab70dfed
Bruno-BarrosBM <bruno_abarros@hotmail.com>
2019-04-28 18:24:01 -0300

    Referenciando os posts do usuário

--- src/main/java/com/brunobarros/workshopmongo/config/Instantiation.java
+++ src/main/java/com/brunobarros/workshopmongo/config/Instantiation.java
@@ -21,7 +21,7 @@ public class Instantiation implements CommandLineRunner {
 	private UserRepository userRepository;
 	
 	@Autowired
-	private PostRepository postReposiroty;
+	private PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -30,7 +30,7 @@ public class Instantiation implements CommandLineRunner {
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
-		postReposiroty.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
@@ -41,7 +41,10 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
-		postReposiroty.saveAll(Arrays.asList(post1, post2));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
